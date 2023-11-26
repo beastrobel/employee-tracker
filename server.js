4594ca94bbb7b43e38f92bd996f51a0e38f56f4a
beastrobel <beatrizstrobel@proton.me>
2023-11-25 23:14:54 -0500

    Completed prompts

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ inquirer
 ])
 
 .then((data) => {
-    console.log('success');
     if (data.main == 'view all departments'){
 
     } else if (data.main == 'view all roles'){
@@ -27,14 +26,64 @@ inquirer
     } else if (data.main == 'view all employees'){
 
     } else if (data.main == 'add a department'){
-
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the department?',
+                name: 'newDepartment'
+            }
+        ])
+        .then((data) => {
+        console.log('New deparment added.');
+        });
     } else if (data.main == 'add a role'){
-
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the role?',
+                name: 'newRole'
+            },
+            {
+                type: 'input',
+                message: 'What is the salary of the role?',
+                name: 'salary'
+            },
+            {
+                type: 'input',
+                message: 'What department does the role belong to?',
+                name: 'department'
+            },
+        ])
+        .then((data) => {
+        console.log('New role added.');
+        });
     } else if (data.main == 'add an employee'){
-
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is their first name?',
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: 'What is their last name?',
+                name: 'lastName'
+            },
+            {
+                type: 'input',
+                message: 'What is their role?',
+                name: 'role'
+            },
+        ])
+        .then((data) => {
+        console.log('New employee added.');
+        });
     } else if (data.main == 'update an employee role'){
-
+        console.log('Feature coming soon.');
     }
 }
 
-);
+); 
